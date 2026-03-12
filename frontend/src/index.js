e1cdf2bce965318efc3677b03f6965e958e4b24f
Auntor101 <auntorchakma@gmail.com>
2026-03-13 01:28:14 +0600

    vercel deployment

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import {Provider} from 'react-redux'
 import store from './store'
 import App from './App';
 import "./App.css"
+
+if (process.env.REACT_APP_API_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+}
 
 ReactDOM.render(
   <Provider store = {store}>
